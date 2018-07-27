90c63a2cebd500cfcb39b9eddde1970563a7075a
Husain Shoab <hsnscent@gmail.com>
2018-07-27 19:13:48 +0530

    Update FrequencySort.java

--- Arrays/FrequencySort.java
+++ Arrays/FrequencySort.java
@@ -16,9 +16,9 @@ class FrequencySort {
     }
  
     public static void main (String args[]) throws java.lang.Exception {
-      Scanner sc = new Scanner(System.in);
-	    int T = sc.nextInt();
-	    while (T-- >0) {
+	Scanner sc = new Scanner(System.in);
+	int T = sc.nextInt();
+	while (T-- >0) {
           int N = sc.nextInt();
           int a[] = new int[N];
 
