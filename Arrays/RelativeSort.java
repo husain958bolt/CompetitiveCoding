10e4249ad3d0eb28a60df98b47a417691ebbfb27
Husain Shoab <hsnscent@gmail.com>
2018-07-27 18:56:12 +0530

    Create RelativeSort.java

---
+++ Arrays/RelativeSort.java
@@ -0,0 +1,60 @@
+/*package whatever //do not write package name here */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+//In CustomComparartot use everything wrt Objects i.e use Wrapper Classes of Primitive Types
+
+class CustomComparator implements Comparator<Integer>{
+    Map<Integer,Integer> hm; 
+    public CustomComparator(Map<Integer,Integer> hm) {
+        this.hm = hm;
+    }
+    
+    @Override
+    public int compare(Integer A,Integer B) {
+        if (hm.containsKey(A) && hm.containsKey(B))
+            return hm.get(A)-hm.get(B);
+        else if (hm.containsKey(A))
+			return -1;
+		else if (hm.containsKey(B))
+			return 1;
+		else
+			return A - B;
+    }
+}
+class RelativeSort {
+
+	public static void main (String[] args) {
+				
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		
+		while (T-- >0) {
+		    int M = sc.nextInt();
+		    int N = sc.nextInt();
+		    
+		    Integer a[] = new Integer[M];
+		    for (int i=0; i<M; i++)
+		        a[i] = sc.nextInt();
+		        
+		    Integer b[] = new Integer[N];
+		    for (int i=0; i<N; i++)
+		        b[i] = sc.nextInt();
+		    
+		    Map<Integer,Integer> hm = new HashMap<Integer,Integer>();
+		    for (int i=0; i<N; i++)
+		        hm.put(b[i],i);
+		        
+		    Arrays.sort(a, new CustomComparator(hm));
+		    
+		    for (int i=0; i<M; i++)
+		        System.out.print(a[i]+" ");
+		    System.out.println();
+		  
+		}
+		
+		
+	}
+}
